40653a14b45938cb68de0432363fa285c880222a
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2024-08-13 11:47:16 -0400

    feat: wip - intros new fork choice rule

--- core/forkchoice.go
+++ core/forkchoice.go
@@ -40,6 +40,88 @@ type ChainReader interface {
 	GetTd(common.Hash, uint64) *big.Int
 }
 
+type ForkChoiceEIP3436 struct {
+	chain ChainReader
+	rand  *mrand.Rand
+
+	// legacy remenant from old forkchoice
+	preserve func(header *types.Header) bool
+}
+
+func NewForkChoiceEIP3436(chain ChainReader, preserve func(header *types.Header) bool) *ForkChoiceEIP3436 {
+	// Seed a fast but crypto originating random generator
+	seed, err := crand.Int(crand.Reader, big.NewInt(math.MaxInt64))
+	if err != nil {
+		log.Crit("Failed to initialize random seed", "err", err)
+	}
+
+	return &ForkChoiceEIP3436{
+		chain:    chain,
+		rand:     mrand.New(mrand.NewSource(seed.Int64())),
+		preserve: preserve,
+	}
+}
+
+// ReorgNeeded returns whether the reorg should be applied
+// based on the given external header and local canonical chain.
+func (f *ForkChoiceEIP3436) ReorgNeeded(current *types.Header, extern *types.Header) (bool, error) {
+	var (
+		localTD  = f.chain.GetTd(current.Hash(), current.Number.Uint64())
+		externTD = f.chain.GetTd(extern.Hash(), extern.Number.Uint64())
+	)
+	if localTD == nil || externTD == nil {
+		return false, errors.New("missing td")
+	}
+
+	// Rule 1: Choose the block with the most total difficulty.
+	if localTD.Cmp(externTD) > 0 {
+		return false, nil
+	} else if localTD.Cmp(externTD) < 0 {
+		return true, nil
+	}
+
+	// Rule 2: Choose the block with the lowest block number.
+	if current.Number.Cmp(extern.Number) < 0 {
+		return false, nil
+	} else if current.Number.Cmp(extern.Number) > 0 {
+		return true, nil
+	}
+
+	// Rule 3: Choose the block whose validator had the least recent in-turn block assignment.
+	// (header_number - validator_index) % validator_count
+	// TODO(@ckartik): Figure out how to get the validator count and most recent in-turn block assignment
+	validatorCount := f.chain.Config().Clique.Validators
+	currentValidatorIndex := int(current.Coinbase.Big().Int64()) % validatorCount
+	externValidatorIndex := int(extern.Coinbase.Big().Int64()) % validatorCount
+
+	currentInTurn := (current.Number.Uint64() - uint64(currentValidatorIndex)) % uint64(validatorCount)
+	externInTurn := (extern.Number.Uint64() - uint64(externValidatorIndex)) % uint64(validatorCount)
+
+	if currentInTurn > externInTurn {
+		return false, nil
+	} else if currentInTurn < externInTurn {
+		return true, nil
+	}
+
+	// Rule 4: Choose the block with the lowest hash.
+	currentHash := new(big.Int).SetBytes(current.Hash().Bytes())
+	externHash := new(big.Int).SetBytes(extern.Hash().Bytes())
+
+	if currentHash.Cmp(externHash) < 0 {
+		return false, nil
+	} else if currentHash.Cmp(externHash) > 0 {
+		return true, nil
+	}
+
+	log.Warn("Potential deadlock state detected: all fork choice rules resulted in a tie",
+		"currentTD", localTD, "externTD", externTD,
+		"currentNumber", current.Number, "externNumber", extern.Number,
+		"currentInTurn", currentInTurn, "externInTurn", externInTurn,
+		"currentHash", currentHash, "externHash", externHash)
+
+	return false, nil
+}
+
 // ForkChoice is the fork chooser based on the highest total difficulty of the
 // chain(the fork choice used in the eth1) and the external fork choice (the fork
 // choice used in the eth2). This main goal of this ForkChoice is not only for
